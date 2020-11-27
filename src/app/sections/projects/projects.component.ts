47d5f91133a981e006fc45b7a63a4a5851fbc7bc
akmalkrmv <akmalkarimov94@gmail.com>
2020-11-27 14:09:58 +0300

    projects first try

--- src/app/sections/projects/projects.component.ts
+++ src/app/sections/projects/projects.component.ts
@@ -4,13 +4,32 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProjectsComponent implements OnInit {
+export class ProjectsComponent {
+  public projects = [
+    {
+      title: 'Uno',
+      subtitle: 'WebRTC video chat',
+      image: 'assets/images/pc-video-chat-black.jpg',
+      link: 'https://uno-app.web.app',
+      description: `
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod rem
+        obcaecati facere quisquam natus neque aliquid cumque, eligendi illo
+        maiores suscipit mollitia. Accusamus quia harum velit quam laborum
+        optio!`,
+    },
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    {
+      title: 'Battleship',
+      subtitle: 'Real time multiplayer game',
+      image: 'assets/images/battleship_1.jpg',
+      link: 'https://battleship-uno.web.app',
+      description: `
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod rem
+        obcaecati facere quisquam natus neque aliquid cumque, eligendi illo
+        maiores suscipit mollitia. Accusamus quia harum velit quam laborum
+        optio!`,
+    },
+  ];
 }
