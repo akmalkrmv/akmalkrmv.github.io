1beea99c81aeb86b1f7afd2be87129519b2b975b
akmalkrmv <akmalkarimov94@gmail.com>
2020-11-27 14:55:51 +0300

    added projects description and tags

--- src/app/sections/projects/projects.component.ts
+++ src/app/sections/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
@@ -13,11 +13,13 @@ export class ProjectsComponent {
       subtitle: 'WebRTC video chat',
       image: 'assets/images/pc-video-chat-black.jpg',
       link: 'https://uno-app.web.app',
+      tools: ['Angular', 'TypeScript', 'Firebase', 'WebRTC'],
       description: `
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod rem
-        obcaecati facere quisquam natus neque aliquid cumque, eligendi illo
-        maiores suscipit mollitia. Accusamus quia harum velit quam laborum
-        optio!`,
+        Side project for understanding how WebRTC works. 
+        It contain audio-video communication using browser API,
+        no frameworks used, to understand how it really works under the hood.
+        It has also message chat, implemented using firabase realtime database.
+        Firebase also used for authentification. Ongoing open source project.`,
     },
 
     {
@@ -25,11 +27,13 @@ export class ProjectsComponent {
       subtitle: 'Real time multiplayer game',
       image: 'assets/images/battleship_1.jpg',
       link: 'https://battleship-uno.web.app',
+      tools: ['Angular', 'TypeScript', 'Firebase'],
       description: `
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod rem
-        obcaecati facere quisquam natus neque aliquid cumque, eligendi illo
-        maiores suscipit mollitia. Accusamus quia harum velit quam laborum
-        optio!`,
+        Battleship game implemented using Firebase for signalling, 
+        and Andgular for UI. It has match making system to connect and find created games.
+        Design for this project was inspired by 
+        <a href="https://www.youtube.com/watch?v=G6JTM-zt-dQ" target="_blank">Web Dev Simplified</a> video,
+        but logic was coded entirely from scratch.`,
     },
   ];
 }
