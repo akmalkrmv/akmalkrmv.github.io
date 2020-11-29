2c0656de43472f02d636580eb437c88e4aa02cbc
akmalkrmv <akmalkarimov94@gmail.com>
2020-11-29 23:03:27 +0300

    carousel using tabs

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CarouselComponent implements OnInit {
+export class CarouselComponent {
+  @Input() images: string[] = [];
+  public selectedIndex: number = 0;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public next() {
+    this.selectedIndex++;
+    this.selectedIndex %= this.images.length;
   }
 
+  public prev() {
+    this.selectedIndex++;
+    this.selectedIndex %= this.images.length;
+  }
 }
