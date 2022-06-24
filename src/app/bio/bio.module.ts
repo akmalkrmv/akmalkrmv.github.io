81ff618361cb07261bfa270242451dff517aee2b
akmalkrmv <akmalkarimov94@gmail.com>
2022-06-24 05:48:47 +0500

    thinner scrollbar

--- src/app/bio/bio.module.ts
+++ src/app/bio/bio.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { MatRippleModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
@@ -49,6 +51,8 @@ import { AboutComponent } from './sections/about/about.component';
     MatButtonModule,
     MatFormFieldModule,
     MatAutocompleteModule,
+    MatRippleModule,
+    MatDividerModule,
   ],
 })
 export class BioModule {}
